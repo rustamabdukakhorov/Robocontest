bb5d31588d4e896fba22466b44af3a5366098339
rustam_abdukakhorov <rustamabdukakhorov@gmail.com>
2022-03-02 22:26:30 +0500

    02.03.2022

---
+++ 520.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 28 10:11:26 2022
+
+@author: R.U.S.T.E.A.M
+"""
+
+n = int(input())
+print(1)
---
+++ 524.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 28 10:05:42 2022
+
+@author: R.U.S.T.E.A.M
+"""
+
+n = int(input())
+for i in range(n):
+    s = input()
+print(n)
